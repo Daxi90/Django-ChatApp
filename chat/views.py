239e694e6cfb7aeb1757e842d791892f7ce60024
Daxi90 <hdateam90@gmail.com>
2024-03-27 13:49:08 +0100

    First usable version

--- chat/views.py
+++ chat/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .models import Message, Chat
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -25,7 +26,33 @@ def login_view(request):
         if user is not None:
             login(request, user)
             # Redirect to a success page.
-            return HttpResponseRedirect(request.POST.get('redirect'))
+            if redirect is not None:
+                return HttpResponseRedirect(request.POST.get('redirect'))
+            else:
+                return HttpResponseRedirect('/chat')
         else:
             return render(request, 'auth/login.html',{'wrongPassword': True, 'redirect': redirect})
     return render(request, 'auth/login.html', {'redirect': redirect})
+
+def register_view(request):
+    redirect = request.GET.get('next')
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        email = request.POST["email"]
+        first_name = request.POST["first_name"]
+        last_name = request.POST["last_name"]
+
+        user = User.objects.create_user(username, email, password)
+        user.first_name = first_name
+        user.last_name = last_name
+        user.save()
+        return HttpResponseRedirect('/login')
+    else:
+        pass
+    return render(request, 'register/index.html', {'redirect': redirect})
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect('/login')
+    # Redirect to a success page.
